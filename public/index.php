71e61b600fea637ed3800ec29d8d3ff099f4f390
dengjiangg <2856986090@qq.com>
2019-07-04 18:15:43 +0800

    我的

--- public/index.php
+++ public/index.php
@@ -28,13 +28,7 @@ if (preg_match("/(.*)\.zhifengwangluo\.com/i", HTTP_HOST, $matches)) {
         '127.0.0.1:10058' => 'home',
         '127.0.0.1:10057' => 'sapi',
         '127.0.0.1:10056' => 'api',
-<<<<<<< HEAD
-        '127.0.0.1:20019' => 'api',
-=======
         '127.0.0.1:20019' => 'admin',
-        'www.retail.com' => 'admin',
-        'api.retail.com' => 'api',
->>>>>>> a1a58c009c70d2f28d90720c087b11d1472f616d
     ];
     if (!empty($terrace[HTTP_HOST])) {
         $module = $terrace[HTTP_HOST];
